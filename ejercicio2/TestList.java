ceab38c098b4fec17cea3159a0eae6d6a7c414a1
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-06-03 18:07:00 -0500

    Agregando una Pila de 2 elementos

--- ejercicio2/TestList.java
+++ ejercicio2/TestList.java
@@ -1,7 +1,27 @@
-import testlist.myExceptions.ExceptionNoFound;
+import myExceptions.ExceptionNoFound;
 
 public class TestList {
     public static void main(String[] args) throws ExceptionNoFound {
         
+        StackList<String> Pila= new StackList<>();
+        
+        try{
+            String element = Pila.push("Gato");
+            System.out.println("Agregando con push: "+element);
+            Pila.push("Perro");
+            System.out.println("Agregando con push: Perro");
+            
+
+            System.out.println("La pila es: "+Pila);
+            System.out.println("\nLa Pila esta vacia?: "+Pila.isEmpty());
+            
+
+            System.out.println("El elemento de la cima es: "+Pila.peek());
+            
+           
+            
+        }catch(Exception err){
+            System.out.println(err);
+        }
     }
 }
