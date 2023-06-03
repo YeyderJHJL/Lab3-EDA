028f076613e474334f138a39343613d9889bae5b
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-06-03 18:08:43 -0500

    Agregando metodos para retirar e imprimir

--- ejercicio2/TestList.java
+++ ejercicio2/TestList.java
@@ -6,19 +6,29 @@ public class TestList {
         StackList<String> Pila= new StackList<>();
         
         try{
+            
             String element = Pila.push("Gato");
             System.out.println("Agregando con push: "+element);
             Pila.push("Perro");
             System.out.println("Agregando con push: Perro");
             
-
+            
             System.out.println("La pila es: "+Pila);
             System.out.println("\nLa Pila esta vacia?: "+Pila.isEmpty());
             
-
+            
             System.out.println("El elemento de la cima es: "+Pila.peek());
             
-           
+            
+            System.out.println("Elemento retirado de la Pila: "+Pila.pop());
+            System.out.println("La nueva Pila es: "+Pila);
+            System.out.println("\nElemento retirado de la Pila: "+Pila.pop());
+            System.out.println("La nueva Pila es: "+Pila);
+            System.out.println("\nLa Pila esta vacia?: "+Pila.isEmpty());
+            
+            
+            
+            
             
         }catch(Exception err){
             System.out.println(err);
