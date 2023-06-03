0b45577c6573bde0d54ca708ec2e429df9451fbc
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-06-02 23:46:42 -0500

    Se creo la clase Queue y se le asigno dos atributos head y tail, ademas se agrego el metodo add() que añade un nuevo elemento a la cola

---
+++ ejercicio/Queue.java
@@ -0,0 +1,17 @@
+public class Queue<E> {
+  private Node<E> head;
+  private Node<E> tail;
+
+  public boolean add(E e) {
+    Node<E> newNode = new Node<E>(e);
+    if (head != null) {
+      tail.setNext(newNode);
+      tail = newNode;
+      return true;
+    } else {
+      head = newNode;
+      tail = newNode;
+      return true;
+    }
+  }
+}
