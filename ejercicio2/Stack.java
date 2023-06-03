5788976cd01c8a2bd03a01ca431cfdc96d4c2b22
PaoMarroquin <pmarroquinpi@unsa.edu.pe>
2023-06-03 10:48:54 -0500

    Stack

--- ejercicio2/Stack.java
+++ ejercicio2/Stack.java
@@ -1,6 +1,9 @@
 import testlist.myExceptions.ExceptionNoFound;
 
 public interface Stack<E> {
-    //metodos de Stack
-    
+  //metodos de Stack
+    public E peek() throws ExceptionNoFound;
+    public E pop() throws ExceptionNoFound;
+    public E push(E e);
+    public int search(Object o);    
 }
